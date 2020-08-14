4ef473b2d34a3be77c3be1a4e149a0497d42f55e
JuanCalMun <juancalmun@gmail.com>
2020-08-14 20:12:40 +0200

    Created entity variety

---
+++ src/main/kotlin/com/coffeeadvisor/coffeeadvisorback/variety/domain/Variety.kt
@@ -0,0 +1,15 @@
+package com.coffeeadvisor.coffeeadvisorback.variety.domain
+
+import com.sun.istack.NotNull
+import java.util.*
+import javax.persistence.*
+
+@Entity
+data class Variety(
+        @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "variety_seq")
+        @SequenceGenerator(name = "variety_seq", sequenceName = "VARIETY_SEQ", allocationSize = 100)
+        var id: Long,
+        @NotNull
+        var uid: UUID,
+        var name: String
+)
