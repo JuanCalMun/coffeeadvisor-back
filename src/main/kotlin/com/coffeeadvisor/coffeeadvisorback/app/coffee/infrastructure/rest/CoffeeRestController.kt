b08466d4a43acc0a59fcadf76ce9dad35b937cdc
JuanCalMun <juancalmun@gmail.com>
2020-08-15 12:55:01 +0200

    Implemented GET by UUID

--- src/main/kotlin/com/coffeeadvisor/coffeeadvisorback/app/coffee/infrastructure/rest/CoffeeRestController.kt
+++ src/main/kotlin/com/coffeeadvisor/coffeeadvisorback/app/coffee/infrastructure/rest/CoffeeRestController.kt
@@ -1,13 +1,18 @@
 package com.coffeeadvisor.coffeeadvisorback.app.coffee.infrastructure.rest
 
-import com.coffeeadvisor.coffeeadvisorback.app.coffee.application.services.CoffeeProvider
+import com.coffeeadvisor.coffeeadvisorback.app.coffee.application.CoffeeProvider
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("api/v1/coffee")
 class CoffeeRestController(private val coffeeProvider: CoffeeProvider) {
     @GetMapping
     fun getAllCoffees() = coffeeProvider.getAll()
+
+    @GetMapping("/{coffeeUUID}")
+    fun getCoffeeByUuid(@PathVariable coffeeUUID: UUID) = coffeeProvider.getOne(coffeeUUID)
 }
